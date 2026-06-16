733ed5d1a6dd5298e6b8f69bcaeaba4fbc3c2a7b
爱莉 <3242431060@qq.com>
2026-06-16 23:10:41 +0800

    fix: registerPlugin before super.onCreate() for Capacitor 6
    Capacitor 6 requires plugins be registered before BridgeActivity.onCreate()

--- android/app/src/main/java/com/learning/platform/MainActivity.java
+++ android/app/src/main/java/com/learning/platform/MainActivity.java
@@ -1,34 +1,13 @@
 package com.learning.platform;
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Bundle;
-import androidx.core.content.FileProvider;
-import android.util.Log;
-
 import com.getcapacitor.BridgeActivity;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import android.os.Bundle;
 
 public class MainActivity extends BridgeActivity {
-    private static final String TAG = "MainActivity";
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(LlamaPlugin.class);
         registerPlugin(AppUpdatePlugin.class);
-        registerPlugin(IntentHandlerPlugin.class);
         super.onCreate(savedInstanceState);
-        IntentHandlerPlugin.init(this);
-        IntentHandlerPlugin.handleIntent(this, getIntent());
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        IntentHandlerPlugin.handleIntent(this, intent);
     }
 }
